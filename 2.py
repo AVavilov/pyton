b4231de1662d08d4c0765365cb9f16551eb3f654
Alexandr <ghostvil@ya.ru>
2021-01-10 20:27:02 +0300

    Исправление ДЗ

--- 2.py
+++
@@ -1,7 +0,0 @@
-# Долго просидел, но не знаю решения
-
-time = int(input('Введите кол-во секунд - '))
-hours = time // 3600
-minutes = (time // 60) - (hours * 60)
-seconds = time % 60
-print(f'{hours:02}:{minutes:02}:{seconds:02}')
